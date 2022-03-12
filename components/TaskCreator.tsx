8c3b3f808993c0e0dfb364fcec49f5d1aa7f2a08
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-03-12 19:09:54 +0800

    Fixed linting issues in TaskCreator.tsx

--- components/TaskCreator.tsx
+++ components/TaskCreator.tsx
@@ -1,32 +1,44 @@
-import React from 'react';
-import { StyleSheet, TextInput, Button, View, ViewStyle} from "react-native";
-
-export default function TaskCreator(props:{onAdd:Function, style?:ViewStyle}):JSX.Element {
-    const [text,onChangeText] = React.useState("");
-
-    function createTask(name:string):Task {
-        const currentDateTime = new Date(Date.now()).toISOString();
-        const key = name + currentDateTime;
-        return {key:key, name:name, done:false}
-    }
-
-    return (
-        <View style={[styles.container, props.style]}>
-            <TextInput style={styles.input} onChangeText={onChangeText} value={text} />
-            <Button onPress={() => props.onAdd(createTask(text))} title="Add" />
-        </View>
-    );
-}
+import { useState } from "react";
+import { StyleSheet, TextInput, Button, View, ViewStyle } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-        flexDirection:"row",
-        maxHeight:37,
+        flex: 1,
+        flexDirection: "row",
+        maxHeight: 37,
     },
     input: {
-        flex:2,
-        paddingLeft:10,
         borderWidth: 1,
+        flex: 2,
+        paddingLeft: 10,
     },
 });
+
+interface Props {
+    onAdd: (newTask: Task) => void;
+    style?: ViewStyle;
+}
+
+export default function TaskCreator({
+    onAdd,
+    style = styles.container,
+}: Props): JSX.Element {
+    const [text, onChangeText] = useState("");
+
+    function createTask(name: string): Task {
+        const currentDateTime = new Date(Date.now()).toISOString();
+        const key = name + currentDateTime;
+        return { key, name, done: false };
+    }
+
+    return (
+        <View style={[styles.container, style]}>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={text}
+            />
+            <Button onPress={() => onAdd(createTask(text))} title="Add" />
+        </View>
+    );
+}
