ab86a5dc460f5a19488ef74ed6da8a5968947aa6
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-03-12 18:08:38 +0800

    Fixed TypeScript support for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,37 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-    "react-native/react-native": true,
-  },
-  extends: ["plugin:react/recommended", "airbnb", "plugin:react-native/all", "prettier"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        es2021: true,
+        node: true,
+        "react-native/react-native": true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint", "react-native"],
-  rules: {},
-}
+    extends: [
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "airbnb",
+        "airbnb-typescript",
+        "airbnb/hooks",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:react-native/all",
+        "prettier",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: ["./tsconfig.eslint.json"],
+        react: {
+            version: "detect",
+        },
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react", "react-native", "@typescript-eslint"],
+    rules: {
+        "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }],
+        "import/extensions": ["error", "never"],
+        "react/jsx-uses-react": "off", // Not required since React 17
+        "react/react-in-jsx-scope": "off",
+    },
+};
